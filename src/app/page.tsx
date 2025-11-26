108769218b47f46dfd610025112bcc3d43ab91c9
manjesh <manjeshrv592@gmail.com>
2025-11-26 15:02:00 +0530

    Redeployment push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 export default function DashboardPage() {
   return (
     <div>
+      {/* Comment */}
      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
        <article className="p-4 bg-white rounded-lg border border-neutral-200 w-full">
         <div className="font-semibold text-neutral-500 mb-2">Total Members</div>
