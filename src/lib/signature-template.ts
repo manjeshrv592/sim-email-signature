2f0491fe8ec4e7e6101dd6759b4d09a7a6732b30
manjesh <manjeshrv592@gmail.com>
2025-12-02 13:00:57 +0530

    Made divider line thinner

--- src/lib/signature-template.ts
+++ src/lib/signature-template.ts
@@ -9,83 +9,62 @@ interface SignatureData {
 export function generateSignatureHtml(data: SignatureData): string {
   return `
     <br/><br/>
-    <table style="font-family: Arial, Helvetica, sans-serif">
+    <table cellpadding="0" cellspacing="0" border="0" style="font-family: Arial, Helvetica, sans-serif">
       <tr>
-        <td style="position: relative">
-          <table>
-            <tr>
-              <td>
-                <span
-                  style="
-                    position: absolute;
-                    width: 1px;
-                    height: 100%;
-                    background-color: #d4d4d4;
-                    top: 0;
-                    right: 20px;
-                  "
-                ></span>
-                <div style="padding-right: 50px">
-                  <div
-                    style="
-                      font-weight: bold;
-                      font-size: 20px;
-                      margin-bottom: 4px;
-                    "
-                  >
-                    ${data.firstName} ${data.lastName}
-                  </div>
-                  <div style="font-size: 14px; margin-bottom: 4px">
-                    ${data.designation}
-                  </div>
-                  <div style="font-size: 14px; color: #737373">
-                    Ph - ${data.contactNumber}
-                  </div>
-                </div>
-              </td>
-            </tr>
-          </table>
+        <td style="padding-right: 20px; vertical-align: middle;">
+          <div>
+            <div
+              style="
+                font-weight: bold;
+                font-size: 20px;
+                margin-bottom: 4px;
+              "
+            >
+              ${data.firstName} ${data.lastName}
+            </div>
+            <div style="font-size: 14px; margin-bottom: 4px">
+              ${data.designation}
+            </div>
+            <div style="font-size: 14px; color: #737373">
+              Ph - ${data.contactNumber}
+            </div>
+          </div>
         </td>
-        <td>
-          <table>
+        <td style="padding-left: 20px; vertical-align: top; border-left: 1px solid #d4d4d4;">
+          <table cellpadding="0" cellspacing="0" border="0">
             <tr>
               <td>
-                <table>
+                <table cellpadding="0" cellspacing="0" border="0">
                   <tr>
-                    <td>
-                      <span>
-                        <div>
-                          <img
-                            src="https://sim-email-signature.vercel.app/simtech-logo.png"
-                            alt="Simtech Logo"
-                            style="height: 56px; width: auto; display: block"
-                          />
-                        </div>
-                        <div
-                          style="
-                            font-size: 14px;
-                            padding-left: 26px;
-                            margin-top: -8px;
-                          "
-                        >
-                          Gopalan Signature Tower
-                        </div>
-                      </span>
-                    </td>
-                    <td>
-                      <span>
+                    <td style="vertical-align: bottom;">
+                      <div>
                         <img
-                          src="https://sim-email-signature.vercel.app/iso-logo.png"
-                          alt="ISO Logo"
-                          height="64"
-                          style="
-                            height: 64px;
-                            width: auto;
-                            display: block;
-                            margin-left: 48px;
-                          "
+                          src="https://sim-email-signature.vercel.app/simtech-logo.png"
+                          alt="Simtech Logo"
+                          style="height: 56px; width: auto; display: block"
                         />
-                      </span>
+                      </div>
+                      <div
+                        style="
+                          font-size: 14px;
+                          padding-left: 26px;
+                          margin-top: -8px;
+                        "
+                      >
+                        Gopalan Signature Tower
+                      </div>
+                    </td>
+                    <td style="vertical-align: bottom; padding-left: 48px;">
+                      <img
+                        src="https://sim-email-signature.vercel.app/iso-logo.png"
+                        alt="ISO Logo"
+                        height="64"
+                        style="
+                          height: 64px;
+                          width: auto;
+                          display: block;
+                        "
+                      />
                     </td>
                   </tr>
                 </table>
@@ -93,7 +72,7 @@ export function generateSignatureHtml(data: SignatureData): string {
             </tr>
             <tr>
               <td>
-                <div style="padding-left: 26px">
+                <div style="padding-left: 30px; padding-top: 8px;">
                   <div style="font-size: 14px; margin-bottom: 4px">
                     3rd, Floor, No.6, Old Madras Road, Nagavarapalya,
                   </div>
